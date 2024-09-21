c5118f4343f2c196d701a1efce5e54098376b484
paigauresh <paigauresh@gmail.com>
2024-09-21 21:33:56 +0530

    sitekey changed

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -73,7 +73,7 @@ export function Form() {
           errors={state.errors}
         />
         <ReCAPTCHA
-          sitekey="6Lfj9NYfAAAAAP8wPLtzrsSZeACIcGgwuEIRvbSg"
+          sitekey="6LfRLUsqAAAAABhkeDu9h1cfTy8dKNDaYnMWdKXf"
           onChange={(e) => {
             setIsHuman(true)
           }}
